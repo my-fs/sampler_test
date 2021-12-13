0c58fdef4209cd588a23e061d010c637eb93271d
aidan <aidan.c.swank@gmail.com>
2021-12-13 15:26:02 +0000

    broken, trying to create sampleosc

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,63 @@ int winHeight = 240;
 
 struct AudioData
 {
-    Uint8* buf;
+    Uint8 *buf;
     Uint32 len;
+};
+
+struct SampleOsc
+{
+    // AudioData data;
+    Uint8 *bufEnd;
+    Uint8 *bufStart;
+    Uint8 *bufPos;
+    float pitch=1;
+    void loadWAV(const std::string &fileName)
+    {
+        // TODO: Handle streamFromFile
+        SDL_AudioSpec wavSpec;
+        Uint8 *wavStart;
+        Uint32 wavLength;
+
+        if (SDL_LoadWAV(fileName.c_str(), &wavSpec, &wavStart, &wavLength) == NULL)
+        {
+            // TODO: Proper error handling
+            std::cout << "Error: " << fileName
+                      << " could not be loaded as an audio file" << std::endl;
+            // throw fileName;
+        }
+
+        bufPos = wavStart;
+        bufStart = wavStart;
+        bufEnd = bufStart + wavLength; //byte offset
+    }
+    void generateSamples(float *stream, size_t len, size_t pos)
+    {
+        bufPos = bufStart + pos;
+
+        if (bufPos >= bufEnd || bufPos < bufStart)
+        {
+            // end
+            // return (size_t)-1;
+        }
+
+        Uint32 length = (Uint32)len;
+        Uint32 lengthLeft = (Uint32)((bufEnd - bufPos)/pitch);
+
+        // audio samples
+        Sint16 *samples = (Sint16 *)bufPos;
+        float sampleIndex = 0;
+        float factor = 1.0f / 32768.0f; // 1/(biggest number in a 16 bit unsigned int)
+        for (Uint32 i = 0; i < len; i++)
+        {
+            stream[i] = (samples[(size_t)sampleIndex]) * factor;
+            // if(playIndex>)
+            sampleIndex += pitch;
+        }
+
+        // audio->buf = audio->buf + length;
+        // audio->len = audio->len - length;
+    }
 };
 
 float *floatStream;
@@ -18,42 +73,44 @@ int sLen;
 
 void myAudioCallback(void *userData, Uint8 *streamIn, int streamInLen)
 {
-    AudioData* audio = (AudioData*)userData;
+    // SampleOsc *osc = (SampleOsc *)userData;
+    AudioData *audio = (AudioData *)userData;
 
     // std::cout << streamLen << std::endl;
-    int streamLen = streamInLen/2; // if len=10, it wants you to fill a buffer of ten 8-bit bytes, but “out” is an array of 16-bit ints
+    int streamLen = streamInLen / 2; // if len=10, it wants you to fill a buffer of ten 8-bit bytes, but “out” is an array of 16-bit ints
 
-    if(audio->len==0)
+    if (audio->len == 0)
     {
         return;
     }
 
-	Uint32 length = (Uint32)streamInLen;
+    Uint32 length = (Uint32)streamInLen;
 
-	if(length > audio->len){
-		length = audio->len;
-	} else {
-		length = length;
-	}
+    if (length > audio->len)
+    {
+        length = audio->len;
+    }
 
     // memcpy(streamIn, audio->buf, length);
 
     floatStream = new float[streamLen]();
     sLen = streamLen; // copy for draw
 
-    //audio samples
-	Sint16* samples = (Sint16*)audio->buf;
-	float sampleIndex = 0;
-	float factor = 1.0f/32768.0f;
-	for(Uint32 i = 0; i < streamLen; i++)
-	{
-		floatStream[i] = (samples[(size_t)sampleIndex]) * factor;
-		sampleIndex += 1;
+    // audio samples
+    Sint16 *samples = (Sint16 *)audio->buf;
+    float sampleIndex = 0;
+    float factor = 1.0f / 32768.0f; // 1/(biggest number in a 16 bit unsigned int)
+    for (Uint32 i = 0; i < streamLen; i++)
+    {
+        floatStream[i] = (samples[(size_t)sampleIndex]) * factor;
+        sampleIndex += 1;
         // std::cout << "float cpy " << floatStream[i] << std::endl;
-	}
+    }
 
+    audio->buf = audio->buf + length;
+    audio->len = audio->len - length;
 
-    //final copy to callback
+    // final copy to callback
     Sint16 *stream = (Sint16 *)streamIn;
     for (size_t i = 0; i < streamLen; i++)
     {
@@ -67,15 +124,11 @@ void myAudioCallback(void *userData, Uint8 *streamIn, int streamInLen)
         {
             val = -1.0f;
         }
-    
+
         // std::cout << val << std::endl;
 
-        stream[i] = (Sint16)(val * 32767); //out stream casted to 16bit integers
+        stream[i] = (Sint16)(val * 32767); // out stream casted to 16bit integers
     }
-
-    audio->buf = audio->buf + length;
-    audio->len = audio->len - length;
-    
 }
 
 int main()
@@ -92,19 +145,23 @@ int main()
     SDL_Texture *cat_texture = SDL_CreateTextureFromSurface(display.renderer, cat_surface);
     SDL_Rect cat_dst = {0, 0, cat_surface->w, cat_surface->h};
 
-	SDL_AudioSpec wavSpec;
-	Uint8* wavBuf;
-	Uint32 wavLength;
+    SDL_AudioSpec wavSpec;
+    Uint8 *wavBuf;
+    Uint32 wavLength;
 
     //"res/AKWF_0014.wav"
 
-	if(SDL_LoadWAV("res/song.wav", &wavSpec, &wavBuf, &wavLength) == NULL)
-	{
-		// TODO: Proper error handling
-		std::cout << "Error: " << "filename" 
-			<< " could not be loaded as an audio file" << std::endl;
-		// throw fileName;
-	}
+    if (SDL_LoadWAV("res/song.wav", &wavSpec, &wavBuf, &wavLength) == NULL)
+    {
+        // TODO: Proper error handling
+        std::cout << "Error: "
+                  << "filename"
+                  << " could not be loaded as an audio file" << std::endl;
+        // throw fileName;
+    }
+
+    SampleOsc oscillator;
+    oscillator.loadWAV("res/song.wav");
 
     AudioData audio;
     audio.buf = wavBuf;
@@ -115,10 +172,10 @@ int main()
 
     // std::cout << wavBuf << std::endl;
     // Sint16* samples = (Sint16*)wavBuf;
-    
+
     // float factor = 1.0f/32768.0f;
     // for(Uint32 i = 0; i < 600; i+=2)
-	// {
+    // {
     //     float samp = samples[i] * factor;
     //     std::cout << i << " " << samp << std::endl;
     // }
@@ -131,7 +188,7 @@ int main()
     spec.channels = 2;
     spec.samples = 512;
     spec.callback = myAudioCallback;
-    spec.userdata = &audio;
+    spec.userdata = &oscillator;
 
     int count = SDL_GetNumAudioDevices(0);
     for (int i = 0; i < count; ++i)
@@ -139,8 +196,8 @@ int main()
         printf("Audio device %d: %s\n", i, SDL_GetAudioDeviceName(i, 0));
     }
 
-    int deviceId= SDL_OpenAudioDevice(SDL_GetAudioDeviceName(1, 0), 0, &spec, NULL, 0);
-    
+    int deviceId = SDL_OpenAudioDevice(SDL_GetAudioDeviceName(1, 0), 0, &spec, NULL, 0);
+
     if (deviceId == 0)
     {
         // TODO: Proper error handling
@@ -159,15 +216,16 @@ int main()
     buffer[2] = 9;
     buffer = buffer + (Uint32)1;
 
-    for(int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++)
     {
-        std::cout << (int)buffer[i] << std::endl;
+        // std::cout << (int)buffer[i] << std::endl;
+        std::cout << (int)*(buffer + i) << std::endl;
     }
 
     SDL_PauseAudioDevice(deviceId, 0);
 
-    bool isRunning=true;
-    while(isRunning)
+    bool isRunning = true;
+    while (isRunning)
     {
         SDL_Event e;
         while (SDL_PollEvent(&e))
@@ -181,25 +239,24 @@ int main()
         SDL_RenderClear(display.renderer);
 
         SDL_RenderCopy(display.renderer, cat_texture, NULL, &cat_dst); // &src is empty cuz it uses whole texture
-    
-        //sine wav in mono so 600 samps instead of 1200
-        //the reft of the samples are garbage? not the other speaker
-        for(Uint32 i = 0; i < sLen; i+=2)
+
+        // sine wav in mono so 600 samps instead of 1200
+        // the reft of the samples are garbage? not the other speaker
+        for (Uint32 i = 0; i < sLen; i += 2)
         {
             float l = floatStream[i];
-            float r = floatStream[i+1];
-            int s = i/(winWidth/100);
+            float r = floatStream[i + 1];
+            int s = i / (winWidth / 100);
             SDL_SetRenderDrawColor(display.renderer, 255, 0, 255, 255);
-            SDL_RenderDrawPoint(display.renderer, s, (l*60)+50);
-            
+            SDL_RenderDrawPoint(display.renderer, s, (l * 60) + 50);
+
             SDL_SetRenderDrawColor(display.renderer, 0, 255, 0, 255);
-            SDL_RenderDrawPoint(display.renderer, s, (r*60)+110);
+            SDL_RenderDrawPoint(display.renderer, s, (r * 60) + 110);
         }
-        
+
         SDL_SetRenderDrawColor(display.renderer, 0, 0, 0, 255);
 
         SDL_RenderPresent(display.renderer);
-
     }
 
     // SDL_PauseAudioDevice(deviceId, 1);
